68758350f48285209529719485a9678ae399ad3f
ENDISit <mr.ilia08@mail.ru>
2024-06-07 00:26:07 +0300

    Добавьте файлы проекта.

---
+++ CurrencyConverter/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+class CurrencyConverter
+{
+    static void Main()
+    {
+        Console.WriteLine("Введите сумму денег для конвертации:");
+        double amount = double.Parse(Console.ReadLine());
+
+        Console.WriteLine("Введите курс конвертации (например, 1.12 для EUR/USD):");
+        double rate = double.Parse(Console.ReadLine());
+
+        double convertedAmount = amount * rate;
+        Console.WriteLine($"Сумма после конвертации: {convertedAmount}");
+
+        Console.WriteLine("Нажмите любую клавишу для выхода...");
+        Console.ReadLine();
+    }
+}
